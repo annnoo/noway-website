012ca128008d6c41a449722307d9207b7d5d1e28
Anno <annnoocodes@gmail.com>
2024-06-01 16:29:43 +0200

    fixed favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark flex min-h-screen w-full flex-col overflow-x-hidden">
+      <head>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+      </head>
 
       <body className={inter.className}>
         <header className=" sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 z-10">
